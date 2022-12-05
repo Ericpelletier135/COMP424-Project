c7148b9394e942423f8c578cd3fffc697907e80d
Eric <ericpelletier135@gmail.com>
2022-12-05 04:13:43 -0500

    cleaned code

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -2,6 +2,8 @@
 from agents.agent import Agent
 from store import register_agent
 import sys
+from time import time
+from copy import deepcopy
 
 
 @register_agent("student_agent")
@@ -21,22 +23,6 @@ class StudentAgent(Agent):
             "l": 3,
         }
         self.moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
-
-    def valid_moves(self, chess_board, my_pos, adv_pos):
-        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
-        max_size = len(chess_board[0])
-        valid_moves = []
-        for mov_dir in range(4): #each direaction
-            my_x, my_y = my_pos
-            if(not chess_board[my_x][my_y][mov_dir]):
-                move_x, move_y = moves[dir]
-                future_x = my_x+move_x
-                future_y = my_y+move_y
-                if((future_x, future_y) != adv_pos):
-                    valid_moves.append((future_x, future_y))
-
-        
-        return valid_moves
 
     def check_valid_step(self, start_pos, end_pos, barrier_dir, chess_board, adv_pos, max_step):
         """
@@ -89,26 +75,6 @@ class StudentAgent(Agent):
         valid_moves = []
         my_x, my_y = my_pos
         max_size = len(chess_board[0])
-        
-        # #putting wall without moving
-        # for mov_dir in range(4): #each direaction
-        #     my_x, my_y = my_pos
-        #     if(not chess_board[my_x][my_y][mov_dir]):
-        #         moves.append((my_pos, mov_dir))
-
-        # initial_moves = self.valid_moves(chess_board, my_pos, adv_pos)
-
-        # for move in initial_moves:
-        #     for i in range(max_step):
-        #         my_x, my_y = move
-        #         for mov_dir in range(4): #each direaction
-        #             if(not chess_board[my_x][my_y][mov_dir]):
-        #                 move_to_add = ((my_x, my_y), mov_dir)
-        #                 if(move_to_add not in moves):
-        #                     moves.append(move_to_add)
-        #     next_move = self.valid_moves(chess_board, next_move, adv_pos)
-
-
 
         for x in range(0, max_step+1): # variable length of move
             for y in range(0, max_step+1-x):
@@ -161,57 +127,22 @@ class StudentAgent(Agent):
                                     if self.check_valid_step(my_pos, end_pos, barrier_dir, chess_board, adv_pos, max_step):
                                         valid_moves.append((end_pos, barrier_dir))
 
-
-
-
-        # for x in range(0, max_step+1): # variable length of move
-            
-        #     for y in range(0, max_step+1-x):
-        #         for dir in range(4):
-        #             if(x == 0):
-        #                 if(y == 0):
-        #                     if(not chess_board[my_x][my_y][dir]):
-        #                             moves.append(((my_x, my_y), dir))
-        #                 else:
-        #                     negative_y = my_y-y
-        #                     positive_y = my_y+y
-        #                     if(negative_y >= 0):    
-        #                         if((not chess_board[my_x][negative_y][dir]) and (my_x, negative_y) != adv_pos):
-        #                             moves.append(((my_x, negative_y), dir))
-        #                         if(positive_y < max_size):    
-        #                             if((not chess_board[my_x][positive_y][dir]) and (my_x, positive_y) != adv_pos):
-        #                                 moves.append(((my_x, positive_y), dir))
-        #             else:
-        #                 negative_x = my_x-x
-        #                 positive_x = my_x+x
-        #                 if(negative_x >= 0):
-        #                     if(y == 0):
-        #                         if((not chess_board[negative_x][my_y][dir]) and (negative_x, my_y) != adv_pos):
-        #                             moves.append(((negative_x, my_y), dir))
-        #                     else:
-        #                         negative_y = my_y-y
-        #                         positive_y = my_y+y
-        #                         if(negative_y >= 0):    
-        #                             if((not chess_board[negative_x][negative_y][dir]) and (negative_x, negative_y) != adv_pos):
-        #                                 moves.append(((negative_x, negative_y), dir))
-        #                             if(positive_y < max_size):    
-        #                                 if((not chess_board[negative_x][positive_y][dir]) and (negative_x, positive_y) != adv_pos):
-        #                                     moves.append(((negative_x, positive_y), dir))
-        #                 if(positive_x < max_size):
-        #                     if(y == 0):
-        #                         if((not chess_board[negative_x][my_y][dir]) and (negative_x, my_y) != adv_pos):
-        #                             moves.append(((negative_x, my_y), dir))
-        #                     else:
-        #                         if(negative_y >= 0):    
-        #                             if((not chess_board[positive_x][negative_y][dir]) and (positive_x, negative_y) != adv_pos):
-        #                                 moves.append(((positive_x, negative_y), dir))
-        #                         if(positive_y < max_size):    
-        #                             if((not chess_board[positive_x][positive_y][dir]) and (positive_x, positive_y) != adv_pos):
-        #                                 moves.append(((positive_x, positive_y), dir))
-                        
-
         return valid_moves
 
+    # def traverse(self, node):
+
+
+    # def rollout(self, node):
+
+    # def backpropagate(self, node):
+
+    # def best_child(self, node):
+
+    # def monte_carlo_tree_search(self, root, start_time):
+    #     while(start_time-time() < 2):
+    #         leaf = self.traverse(root)
+
+        
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
         """
@@ -232,9 +163,15 @@ class StudentAgent(Agent):
 
         # print(max_step)
 
+        start_time = time()
+    
         moves = self.generate_valid_moves(chess_board, my_pos, adv_pos, max_step)
 
+        # Create board states from moves, (update chess_board true false value at location of move and update my_pos to move)
+    
+        print("Time taken: ", time() - start_time)
+
         # dummy return
-        return moves
+        return moves[0]
 
 
